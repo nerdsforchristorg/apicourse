6719416b13feab221168a2cd2bdffa38f83d8e1b
Luke Stuckey <stuckeylu10@gmail.com>
2025-03-19 20:29:25 -0400

    made tasks db

--- apiservice/index.js
+++ apiservice/index.js
@@ -25,8 +25,8 @@ async function createUserTable(db) {
 }
 
 async function createTasksTable(db) {
-    console.log("create Tasks table");
-    return db.exec(`
+  console.log("create Tasks table");
+  return db.exec(`
   CREATE TABLE tasks
   (
     id  VARCHAR(10) NOT NULL,
@@ -42,6 +42,24 @@ async function createTasksTable(db) {
     tags TEXT
     );
 `);
+}
+
+async function tableCheck(db, tableName) {
+  db.get(
+    `SELECT name FROM sqlite_master WHERE type='table' AND name=?`,
+    [tableName],
+    (err, row) => {
+      if (err) {
+        console.error(err.message);
+      } else {
+        if (row) {
+          console.log(`Table "${tableName}" exists.`);
+        } else {
+          console.log(`Table "${tableName}" does not exist.`);
+        }
+      }
+    }
+  );
 }
 
 async function insertRow(db, row) {
@@ -60,10 +78,11 @@ async function insertRow(db, row) {
 }
 
 async function insertTask(db, row) {
-  //   const [name, color, weight] = process.argv.slice(2);
-  console.log("InsertRow Task");
+  console.log("InsertRow Task", row);
   return db.run(
-    `INSERT INTO tasks (id, user_id, title, description, created_at, updated_at, due_date, completed, category, tags, firstName, lastName, email) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+    `INSERT INTO tasks (id, user_id, title, 
+           description, created_at, updated_at,
+           due_date, completed, category_id, tags) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
     row,
     function (error) {
       if (error) {
@@ -74,40 +93,21 @@ async function insertTask(db, row) {
   );
 }
 
-async function insertTask(db,row) {
-       console.log("InsertRow Task",row);
-       return db.run(
-           `INSERT INTO tasks (id, user_id, title, 
-           description, created_at, updated_at,
-           due_date, completed, category_id, tags) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-           row,
-           function (error) {
-               if (error) {
-                   console.error(error.message);
-               }
-               console.log(`Inserted a row with the ID: ${this.lastID}`);
-           }
-       );
-   }
-
-
-
 function getAll() {
- console.log("get all");
- const db = createDbConnection();   
-     db.all(
-        `SELECT * FROM users`, (error, rows) => {
-            if (error) {
-                throw new Error(error.message);
-            }
-            console.log("getAllExit,----:",rows);
-            return rows;
-        });
+  console.log("get all");
+  const db = createDbConnection();
+  db.all(`SELECT * FROM users`, (error, rows) => {
+    if (error) {
+      throw new Error(error.message);
+    }
+    console.log("getAllExit,----:", rows);
+    return rows;
+  });
 }
 
 // function getAllTasks() {
 //     console.log("Get All Tasks");
-//     const db = createDbConnection();   
+//     const db = createDbConnection();
 //         db.all(
 //            `SELECT * FROM tasks`, (error, rows) => {
 //                if (error) {
@@ -117,9 +117,6 @@ function getAll() {
 //                return rows;
 //            });
 //    }
-   
-
-
 
 async function getAllUsers(db) {
   console.log("get All Users");
@@ -150,50 +147,54 @@ async function findUsersByName(db, firstName, lastName) {
   return rows;
 }
 
-
 async function createDbConnection() {
-
-    const db = await open({
-        filename: DBNAME, // Database file
-        driver: sqlite3.Database, // Driver to use from sqlite3
-      });
-      return db;
-  }
-   
- async function getAllTasks(db) {
-        console.log("get All Tasks");
-         // Query all users
-         const rows = await db.all('SELECT * FROM tasks');
-         //console.log('All Users:', rows);
-         return rows;
- } 
- 
-
-async function getOne(db,id) {
-    const result = await db.get('SELECT * FROM users WHERE id = ?', id)
-    return result;
+  const db = await open({
+    filename: DBNAME, // Database file
+    driver: sqlite3.Database, // Driver to use from sqlite3
+  });
+  return db;
 }
 
+async function getAllTasks(db) {
+  console.log("get All Tasks");
+  // Query all users
+  const rows = await db.all("SELECT * FROM tasks");
+  //console.log('All Users:', rows);
+  return rows;
+}
 
-async function getOneTask(db,id) {
-    const result = await db.get('SELECT * FROM tasks WHERE id = ?', id)
-    return result;
+async function getOne(db, id) {
+  const result = await db.get("SELECT * FROM users WHERE id = ?", id);
+  return result;
+}
+
+async function getOneTask(db, id) {
+  const result = await db.get("SELECT * FROM tasks WHERE id = ?", id);
+  return result;
 }
 
 async function initToDoTable() {
-    console.log("Seed todo table");
-    db = await createDbConnection();
-   // await createTasksTable(db);
-   let obj1 = ["1", "1", "Practice 3 Points", "Coach asked me to improve my score",
-      "03/10/2025", "03/10/2025", "03/15/2025",
-      false,
-      "basketball",
-      "improvement"
-    ];
-    await insertTask(db, obj1);
+  console.log("Seed todo table");
+  db = await createDbConnection();
+  const check = await tableCheck(db, "tasks");
+  if (check) {
+    return;
+  }
+  await createTasksTable(db);
+  let obj1 = [
+    "1",
+    "1",
+    "Practice 3 Points",
+    "Coach asked me to improve my score",
+    "03/10/2025",
+    "03/10/2025",
+    "03/15/2025",
+    false,
+    "basketball",
+    "improvement",
+  ];
+  await insertTask(db, obj1);
 }
-
-async function initToDoTable() {}
 
 async function initDb() {
   console.log("Test API App Starting");
@@ -225,13 +226,12 @@ async function openDb() {
 }
 
 async function bootApp() {
+  // db the shared db variable for the database instance
+  console.log("bootapp");
+  await initToDoTable();
 
-// db the shared db variable for the database instance
-console.log("bootapp");
-await initToDoTable();
-
-if (fs.existsSync(DBNAME)) {
-    console.log('SQLite database exists.');
+  if (fs.existsSync(DBNAME)) {
+    console.log("SQLite database exists.");
     const db = await openDb();
     //console.log("db2",db);
     return db;
@@ -297,10 +297,10 @@ app.get("/api/users/:id", async function (req, res) {
   res.json(users);
 });
 
-app.get('/api/tasks',  async function (req, res) {
-    const tasks = await getAllTasks(db);
-    console.log('get--> /api/task',tasks);
-    res.json(tasks);
+app.get("/api/tasks", async function (req, res) {
+  const tasks = await getAllTasks(db);
+  console.log("get--> /api/task", tasks);
+  res.json(tasks);
 });
 
 // get all users (R = CRUD)
@@ -365,106 +365,102 @@ app.put("/api/users", async function (req, res) {
 });
 
 // update partial user (U = CRUD)
-app.patch('/api/users', async function (req, res) {
-    console.log("patch: body", req.body);
-    const id = req.body.id.toString();
-    const email = req.body.email;
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
+app.patch("/api/users", async function (req, res) {
+  console.log("patch: body", req.body);
+  const id = req.body.id.toString();
+  const email = req.body.email;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
 
-    let obj = [firstName,lastName,email,id];
-    console.log("update database",obj);
-    const result = await db.run(
-        'UPDATE users SET firstName = ?, lastName = ?, email = ? WHERE id = ?',
-        obj
-    )
-    res.json(result);
-})
+  let obj = [firstName, lastName, email, id];
+  console.log("update database", obj);
+  const result = await db.run(
+    "UPDATE users SET firstName = ?, lastName = ?, email = ? WHERE id = ?",
+    obj
+  );
+  res.json(result);
+});
 
 // get one user  (R=CRUD)
-app.get('/api/tasks/:id',  async function (req, res) {
-    console.log("get a task by id");
-    console.log(req.params.id);
-    const users = await getTask(db,req.params.id);
-    console.log('get--> /api/users',users);
-    res.json(users);
-})
-
-
+app.get("/api/tasks/:id", async function (req, res) {
+  console.log("get a task by id");
+  console.log(req.params.id);
+  const users = await getTask(db, req.params.id);
+  console.log("get--> /api/users", users);
+  res.json(users);
+});
 
 // create one tasks (C = CRUD)
-app.post('/api/tasks', async function (req, res) {
+app.post("/api/tasks", async function (req, res) {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Methods", "GET,POST");
+  res.set("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
+  console.log("post: body", req.body);
 
-    res.set('Access-Control-Allow-Origin','*');
-    res.set('Access-Control-Allow-Methods','GET,POST');
-    res.set('Access-Control-Allow-Headers','X-Requested-With,Content-Type');
-    console.log("post: body", req.body);
+  const id = req.body.id.toString();
+  const user_id = req.body.user_id;
+  const title = req.body.title;
+  const description = req.body.description;
+  const update_at = new Date();
+  const created_at = new Date();
+  const completed = req.body.completed;
+  const category_id = req.body.category_id;
+  const tags = req.body.tags;
 
-    const id = req.body.id.toString();
-    const user_id = req.body.user_id;
-    const title = req.body.title;
-    const description = req.body.description;
-    const update_at = new Date();
-    const created_at = new Date();
-    const completed = req.body.completed;
-    const category_id= req.body.category_id;
-    const tags = req.body.tags;
-
-    let obj = [user_id,title,description,created_at,completed,category_id,tags,id];
-    console.log("update tags",obj);
-    const result = await db.run(
-        'UPDATE tasks SET user_id = ?, title = ?, description = ?, crated_at = ?, completed = ?, category_id = ?, tags = ? WHERE id = ?',
-        obj
-    )
-    res.json(result);
-
-})
-
-
-
+  let obj = [
+    user_id,
+    title,
+    description,
+    created_at,
+    completed,
+    category_id,
+    tags,
+    id,
+  ];
+  console.log("update tags", obj);
+  const result = await db.run(
+    "UPDATE tasks SET user_id = ?, title = ?, description = ?, crated_at = ?, completed = ?, category_id = ?, tags = ? WHERE id = ?",
+    obj
+  );
+  res.json(result);
+});
 
 // update one user (U = CRUD)
-app.put('/api/tasks', async function (req, res) {
-    console.log("put: tasks", req.body);
-    const id = req.body.id.toString();
-    const user_id = req.body.user_id;
-    const title = req.body.title;
-    const description = req.body.description;
-    const update_at = new Date();
-    const completed = req.body.completed;
-    const category_id= req.body.category_id;
-    const tags = req.body.tags;
+app.put("/api/tasks", async function (req, res) {
+  console.log("put: tasks", req.body);
+  const id = req.body.id.toString();
+  const user_id = req.body.user_id;
+  const title = req.body.title;
+  const description = req.body.description;
+  const update_at = new Date();
+  const completed = req.body.completed;
+  const category_id = req.body.category_id;
+  const tags = req.body.tags;
 
-    let obj = [user_id,title,description,update_at,completed,category_id,tags,id];
-    console.log("update tags",obj);
-    const result = await db.run(
-        'UPDATE tasks SET user_id = ?, title = ?, description = ?, update_at = ?, completed = ?, category_id = ?, tags = ? WHERE id = ?',
-        obj
-    )
-    res.json(result);
-})
-
-
+  let obj = [
+    user_id,
+    title,
+    description,
+    update_at,
+    completed,
+    category_id,
+    tags,
+    id,
+  ];
+  console.log("update tags", obj);
+  const result = await db.run(
+    "UPDATE tasks SET user_id = ?, title = ?, description = ?, update_at = ?, completed = ?, category_id = ?, tags = ? WHERE id = ?",
+    obj
+  );
+  res.json(result);
+});
 
 // Delete one  task
-app.delete('/api/tasks/:id', async function (req, res) {
-    console.log('delete tasks',req.params.id);
-    const id =  req.params.id;
-    const result = await db.run(
-        'DELETE from tasks WHERE id = ?',
-        [id]
-    )
-    console.log("Delete:",result);
-    res.json(result);
-})
-
-
-
-
-
-  let obj = [email, id];
-  console.log("update database", obj);
-  const result = await db.run("UPDATE users SET email = ? WHERE id = ?", obj);
+app.delete("/api/tasks/:id", async function (req, res) {
+  console.log("delete tasks", req.params.id);
+  const id = req.params.id;
+  const result = await db.run("DELETE from tasks WHERE id = ?", [id]);
+  console.log("Delete:", result);
   res.json(result);
 });
 
